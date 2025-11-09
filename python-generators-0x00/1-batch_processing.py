5a78bae138b68fd19632d3a8e6df1b3d2b755d23
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-11-09 23:26:00 +0200

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-"""
-Batch processing module for filtering users by age
-"""
 import seed
 
 
 def stream_users_in_batches(batch_size):
-    """
-    Generator that fetches rows in batches from the database
-    
-    Args:
-        batch_size: Number of rows to fetch per batch
-        
-    Yields:
-        list: Batch of user dictionaries
-    """
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
@@ -34,12 +22,6 @@ def stream_users_in_batches(batch_size):
 
 
 def batch_processing(batch_size):
-    """
-    Processes each batch to filter users over the age of 25
-    
-    Args:
-        batch_size: Size of each batch to process
-    """
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
