b8af0758cbb755c89296c05e4dfb4ccf38a19652
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-11-16 22:32:47 +0200

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
 
+TEST_PAYLOAD = [
+  (
+    {"repos_url": "https://api.github.com/orgs/google/repos"},
+    [
+      {"name": "episodes.dart", "license": {"key": "bsd-3-clause"}},
+      {"name": "cpp-netlib", "license": {"key": "bsl-1.0"}},
+      {"name": "dagger", "license": {"key": "apache-2.0"}},
+      {"name": "ios-webkit-debug-proxy", "license": {"key": "other"}},
+      {"name": "google.github.io", "license": None},
+      {"name": "kratu", "license": {"key": "apache-2.0"}},
+      {"name": "build-debian-cloud"},
+      {"name": "traceur-compiler", "license": {"key": "apache-2.0"}},
+      {"name": "firmata.py", "license": {"key": "apache-2.0"}},
+    ],
+    ['episodes.dart', 'cpp-netlib', 'dagger', 'ios-webkit-debug-proxy', 'google.github.io', 'kratu', 'build-debian-cloud', 'traceur-compiler', 'firmata.py'],
+    ['dagger', 'kratu', 'traceur-compiler', 'firmata.py'],
+  )
+]
