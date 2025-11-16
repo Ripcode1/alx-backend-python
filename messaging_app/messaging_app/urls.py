aadbf84e74680e640b5c4696968fc8eb1441bb29
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-11-16 21:50:32 +0200

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+"""
+URL configuration for messaging_app project.
+"""
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),
+]
