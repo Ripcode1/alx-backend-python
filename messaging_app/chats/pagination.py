9a22dda053978bc28e7946f12b7286e8a7e8a9cf
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-11-23 14:38:14 +0200

    Update pagination.py

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     """
@@ -8,3 +9,14 @@ class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+    
+    def get_paginated_response(self, data):
+        """
+        Custom paginated response that includes page.paginator.count
+        """
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
