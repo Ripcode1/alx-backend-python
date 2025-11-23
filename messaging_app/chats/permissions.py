eb004f0e5fd29fde301097619bbefa843a0c7e35
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-11-23 13:34:17 +0200

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,1 +1,18 @@
+from rest_framework import permissions
 
+class IsParticipantOfConversation(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if hasattr(obj, 'conversation'):
+            return request.user in obj.conversation.participants.all()
+        if hasattr(obj, 'participants'):
+            return request.user in obj.participants.all()
+        return False
+
+class IsMessageSenderOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.sender == request.user
